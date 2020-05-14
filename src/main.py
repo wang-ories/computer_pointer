c96afe5698d7530a4f7a0ac193eec82f762726da
wang-ories <ories.wang@gmail.com>
2020-05-14 12:23:47 +0100

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,32 @@
+# Import
+import numpy as np
+import time
+from openvino.inference_engine import IENetwork, IECore
+import os
+import cv2
+import argparse
+
+
+def main(args):
+    model=args.model
+    device=args.device
+    video_file=args.video
+    max_people=args.max_people
+    threshold=args.threshold
+    output_path=args.output_path
+    print('Model used' , model)
+
+
+if __name__ == '__main__':
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--model', required=True)
+    parser.add_argument('--device', default='CPU')
+    parser.add_argument('--video', default=None)
+    parser.add_argument('--queue_param', default=None)
+    parser.add_argument('--output_path', default='/results')
+    parser.add_argument('--max_people', default=2)
+    parser.add_argument('--threshold', default=0.60)
+
+    args=parser.parse_args()
+
+    main(args)
